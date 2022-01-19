87a5e2887d5f55c495480d3e0292c7e2f48f3714
D-K-Stewart <90126394+D-K-Stewart@users.noreply.github.com>
2022-01-19 15:47:35 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function hide(element) {
+    element.remove();
+}
+
+function turnOff(element) {
+    element.innerText = "Logout";
+}
+
+function custom() {
+    // we can include more code here if we want to
+    console.log("this message is coming from script.js");
+}
